298518bb6f65be0c74e20bbc514aeaf9e4f20c2f
philliam81 <philliam81@gmail.com>
2024-12-20 19:02:22 -0600

    Adding asteroids files

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f" Screen height: {SCREEN_HEIGHT}")
+
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         for event in pygame.event.get():
@@ -20,6 +21,8 @@ def main():
         screen.fill(black)
 
         pygame.display.flip()
+        time =  clock.tick(60)
+        dt = time / 1000
 
 
 
