4c9f8e3c68f64d8472b2d03304b39fd04ff24291
philliam81 <philliam81@gmail.com>
2024-12-21 18:38:47 -0600

    more asteroids

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 from player import Player
 
+
 def main():
     pygame.init()
-
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        Player.update(dt)
-        # Fill the screen with black
-        black = (0, 0, 0)  # RGB color for black
-        screen.fill(black)
 
+        player.update(dt)
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        time =  clock.tick(60)
-        dt = time / 1000
 
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
